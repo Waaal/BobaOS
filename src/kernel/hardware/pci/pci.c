d0340a73651c485d979a997743a8c77d94c7b886
Waaal <luke.duerrbeck@web.de>
2025-05-02 21:26:34 +0200

    Added a PCI scan method for (#22)

--- src/kernel/hardware/pci/pci.c
+++ src/kernel/hardware/pci/pci.c
@@ -1,5 +1,7 @@
 #include "pci.h"
 
+#include <stddef.h>
+
 #include "io/io.h"
 #include "memory/kheap/kheap.h"
 #include "print.h"
@@ -12,35 +14,122 @@ static uint32_t readConfig(uint8_t bus, uint8_t device, uint8_t function, uint8_
 	return ind(PCI_CONFIG_DATA);
 }
 
+static struct pciHeaderDevice* readDeviceHeader(uint8_t bus, uint8_t device, uint8_t function)
+{
+	struct pciHeaderDevice* header = (struct pciHeaderDevice*)kzalloc(sizeof(struct pciHeaderDevice*));
+	
+	uint32_t bar0 = readConfig(bus, device, function, 0x10);
+	uint32_t bar1 = readConfig(bus, device, function, 0x14);
+	uint32_t bar2 = readConfig(bus, device, function, 0x18);
+	uint32_t bar3 = readConfig(bus, device, function, 0x1C);
+	uint32_t bar4 = readConfig(bus, device, function, 0x20);
+	uint32_t bar5 = readConfig(bus, device, function, 0x24);
+
+	uint32_t cisPointer = readConfig(bus, device, function, 0x28);
+	uint32_t subsystemId_subsystemVendorId = readConfig(bus, device, function, 0x2C);
+	uint32_t romAddress = readConfig(bus, device, function, 0x30);
+	uint32_t reserved_pointer = readConfig(bus, device, function, 0x34);
+	uint32_t latency_grant_pin_line = readConfig(bus, device, function, 0x3c);
+	
+	header->BAR0 = bar0;
+	header->BAR1 = bar1;
+	header->BAR2 = bar2;
+	header->BAR3 = bar3;
+	header->BAR4 = bar4;
+	header->BAR5 = bar5;
+
+	header->CisPointer = cisPointer;
+	header->subSystemId = subsystemId_subsystemVendorId >> 16;
+	header->subSystemVendorId = subsystemId_subsystemVendorId;
+	header-> expansionRomAddress = romAddress;
+	header->capabilitiesPointer = reserved_pointer;
+	header->maxLatency = latency_grant_pin_line >> 24;
+	header->minGrant = latency_grant_pin_line >> 16;
+	header->interruptPin = latency_grant_pin_line >> 8;
+	header->interruptLine = latency_grant_pin_line;
+
+	return header;
+}
+
 static struct pciDevice* readPciDevice(uint8_t bus, uint8_t device, uint8_t function)
 {
-	struct pciDevice* pci = (struct pciDevice*)kzalloc(sizeof(struct pciDevice));
+	struct pciDevice* pciDevice = (struct pciDevice*)kzalloc(sizeof(struct pciDevice));
+	struct pciHeader* header = (struct pciHeader*)kzalloc(sizeof(struct pciHeader));
 
 	uint32_t device_vendor = readConfig(bus, device, function, 0x0);
 	uint32_t status_command = readConfig(bus, device, function, 0x4);
 	uint32_t class_subClass_prog_revision = readConfig(bus, device, function, 0x8);
 	uint32_t bist_header_latency_cache = readConfig(bus, device, function, 0xC);
 
-	pci->deviceId = device_vendor >> 16;
-	pci->vendorId = device_vendor;
+	header->deviceId = device_vendor >> 16;
+	header->vendorId = device_vendor;
 
-	pci->status = status_command >> 16;
-	pci->command = status_command;
+	header->status = status_command >> 16;
+	header->command = status_command;
 
-	pci->classCode = class_subClass_prog_revision >> 24;
-	pci->subClass = class_subClass_prog_revision >> 16;
-	pci->prog = class_subClass_prog_revision >> 8;
-	pci->revisionId = class_subClass_prog_revision;
+	header->classCode = class_subClass_prog_revision >> 24;
+	header->subClass = class_subClass_prog_revision >> 16;
+	header->prog = class_subClass_prog_revision >> 8;
+	header->revisionId = class_subClass_prog_revision;
 	
-	pci->bist = bist_header_latency_cache >> 24;
-	pci->headerType = bist_header_latency_cache >> 16;
-	pci->latencyTimer = bist_header_latency_cache >> 8;
-	pci->cacheLineSize = bist_header_latency_cache;
+	header->bist = bist_header_latency_cache >> 24;
+	header->headerType = bist_header_latency_cache >> 16;
+	header->latencyTimer = bist_header_latency_cache >> 8;
+	header->cacheLineSize = bist_header_latency_cache;
+	
+	switch(header->headerType)
+	{
+		case PCI_HEADER_TYPE_DEVICE:
+			{
+				struct pciHeaderDevice* deviceHeader = readDeviceHeader(bus, device, function);
+				header->extendedHeader.deviceHeader = deviceHeader;
+				break;
+			}
+		case PCI_HEADER_TYPE_PCI_BRIDGE:
+			{
+				kprintf("  [PCI-Error: ] No support for pci to pci bridges. Bus %u device %u\n", bus, device);
+				goto error;
+			}
+		case PCI_HEADER_TYPE_CARD_BUS_BRIDGE:
+			{
+				kprintf("  [PCI-Error ]: No support for pci to card bus bridges. Bus %u device %u\n", bus, device);
+				goto error;
+			}
+			case PCI_HEADER_TYPE_MULTI_FUNCTION:
+			{
+				for(uint8_t i = 1; i < 255; i++)
+				{
+					uint16_t vendor = (uint16_t)(readConfig(bus, device, i, 0x0) & 0xFFFF);
+					if(vendor == 0xFFFF){break;}
+					struct pciDevice* subDevice = readPciDevice(bus, device, i);
+					if(subDevice != NULL)
+					{
+						kprintf("  [PCI-SubDevice]: %u Class: %u SubClass: %u\n", pciDevice->header->deviceId, pciDevice->header->classCode, pciDevice->header->subClass);
+					}
+				}
+				break;
+			}
+			default:
+			{
+				kprintf("  [PCI-Error ]: PCI header unknown. Bus %u device %u\n", bus, device);
+				goto error;
+			}
+	}
+	
+	pciDevice->header = header;
+	pciDevice->bus = bus;
+	pciDevice->device = device;
+	pciDevice->function = function;
 
-	return pci;
+	return pciDevice;
+
+error:
+	kzfree(pciDevice);
+	kzfree(header);
+	return (void*)0x0;
 }
 
-void pciInit()
+static void pciScanBus()
 {
 	for(uint8_t bus = 0; bus < 255; bus++)
 	{
@@ -50,12 +139,17 @@ void pciInit()
 			if(vendor != 0xFFFF)
 			{
 				struct pciDevice* pciDevice = readPciDevice(bus, device, 0);
-				kprintf("PCI DEVICE: %u Class: %u SubClass: %u\n", pciDevice->deviceId, pciDevice->classCode, pciDevice->subClass);
+				if(pciDevice != NULL)
+				{
+					kprintf("  [PCI-Device]: %u Class: %u SubClass: %u\n", pciDevice->header->deviceId, pciDevice->header->classCode, pciDevice->header->subClass);
+				}
 			}
 		}
 	}
+}
 
-	struct pciDevice* testDevice = readPciDevice(0,1,0);
-
-	if(testDevice){}
+void pciInit()
+{
+	print("PCI Scan:\n");
+	pciScanBus();
 };
--- src/kernel/hardware/pci/pci.h
+++ src/kernel/hardware/pci/pci.h
@@ -6,7 +6,20 @@
 #define PCI_CONFIG_ADDRESS 0xCF8
 #define PCI_CONFIG_DATA 0xCFC
 
+#define PCI_HEADER_TYPE_DEVICE  0x0
+#define PCI_HEADER_TYPE_PCI_BRIDGE 0x1
+#define PCI_HEADER_TYPE_CARD_BUS_BRIDGE 0x2
+#define PCI_HEADER_TYPE_MULTI_FUNCTION 0x80
+
 struct pciDevice
+{
+	uint8_t bus;
+	uint8_t device;
+	uint8_t function;
+	struct pciHeader* header;
+};
+
+struct pciHeader
 {
 	uint16_t deviceId;
 	uint16_t vendorId;
@@ -20,6 +33,39 @@ struct pciDevice
 	uint8_t headerType;
 	uint8_t latencyTimer;
 	uint8_t cacheLineSize;
+	union
+	{
+		struct pciHeaderDevice* deviceHeader;
+		struct pciHeaderPciBridge* bridgeHeader;
+		struct pciHeaderCardBridge* cardHeader;
+	} extendedHeader;
+};
+
+struct pciHeaderDevice
+{
+	uint32_t BAR0;
+	uint32_t BAR1;
+	uint32_t BAR2;
+	uint32_t BAR3;
+	uint32_t BAR4;
+	uint32_t BAR5;
+	uint32_t CisPointer;
+	uint16_t subSystemId;
+	uint16_t subSystemVendorId;
+	uint32_t expansionRomAddress;
+	uint8_t capabilitiesPointer;
+	uint8_t maxLatency;
+	uint8_t minGrant;
+	uint8_t interruptPin;
+	uint8_t interruptLine;
+};
+
+struct pciHeaderPciBridge
+{
+};
+
+struct pciHeaderCardBridge
+{
 };
 
 void pciInit();
