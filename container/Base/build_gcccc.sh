b27168f6ba2b704a2232ed0c029f6aef5661e8b0
DDCOOL <Kralledd@gmail.com>
2025-06-18 17:08:26 +0200

    de-spammiefied log

--- container/Base/build_gcccc.sh
+++ container/Base/build_gcccc.sh
@@ -14,8 +14,8 @@ mkdir -p builds builds/binutils-2.44 builds/gcc-15.1.0
 curl -o builds/gcc-15.1.0.tar.xz https://mirror.easyname.at/gnu/gcc/gcc-15.1.0/gcc-15.1.0.tar.xz
 curl -o builds/binutils-2.44.tar.xz https://mirror.easyname.at/gnu/binutils/binutils-2.44.tar.xz
 
-tar -xvJf builds/gcc-15.1.0.tar.xz
-tar -xvJf builds/binutils-2.44.tar.xz
+tar -xJf builds/gcc-15.1.0.tar.xz
+tar -xJf builds/binutils-2.44.tar.xz
 
 rm builds/gcc-15.1.0.tar.xz
 rm builds/binutils-2.44.tar.xz
